7e3f6984a607f89598ce8de60f60a0b39cb938c3
sara niha <fathimaniha9845@gmail.com>
2026-02-13 18:53:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-
-import sqlite3
-
-conn = sqlite3.connect("tennis_db.sqlite")
-cursor = conn.cursor()
-cursor.execute("PRAGMA table_info(Competitor_Rankings);")
-print(cursor.fetchall())
-conn.close()
